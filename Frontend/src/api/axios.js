3ed07e5de7790f5c70afe5665abcb46853f746e5
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-11-27 23:36:24 +0530

    Deployment changes

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",  
+  baseURL: "https://task-manager-rho-five-63.vercel.app/api/v1",  
   withCredentials: true 
 });
 
